9781fe320249d7d47fe77721feb8e5e84bd5b9e1
taemi <dmswl1002@naver.com>
2019-08-05 19:34:02 +0900

    검색 안정

--- main/views.py
+++ main/views.py
@@ -16,14 +16,13 @@ class ListView(ListView):
     model = Post
 
 
-
 # 검색 
 class SearchFormView(FormView):
     form_class = SearchForm
     template_name= 'main/list.html'
     
     def form_valid(self, form): #post method로 값이 전달되면
-        form_class = SearchForm # 정의 안 해주면 오류
+        form_class = self.form_class # 정의 안 해주면 오류
         word = '%s' %self.request.POST['word'] # 검색어
         post_list = Post.objects.filter(
             Q(title__icontains=word) | Q(context__icontains=word) # Q 객체를 써서 검색한다. title과 content 칼럼에 대소문자 구분 없이 포함 검사( contains)
