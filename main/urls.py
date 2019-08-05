45dd54c9c940501f4a737d47f8db9a3268cb255e
taemi <dmswl1002@naver.com>
2019-08-05 18:00:50 +0900

    검색 후 검색 창이 다시 안 뜨는 문제 해결 / list와 함치진 않음

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ListView.as_view(), name = 'list'),
+    path('search/', views.SearchFormView.as_view(), name= 'search'),
 ]
--- main/views.py
+++ main/views.py
@@ -10,13 +10,20 @@ from .forms import SearchForm
 from .models import Post
 
 
-class ListView(ListView, FormView):
+class ListView(ListView):
     template_name = 'main/list.html'
     context_object_name = 'post_list'
-    form_class = SearchForm
     model = Post
 
+
+
+# 검색 
+class SearchFormView(FormView):
+    form_class = SearchForm
+    template_name= 'main/list.html'
+    
     def form_valid(self, form): #post method로 값이 전달되면
+        form_class = SearchForm # 정의 안 해주면 오류
         word = '%s' %self.request.POST['word'] # 검색어
         post_list = Post.objects.filter(
             Q(title__icontains=word) | Q(context__icontains=word) # Q 객체를 써서 검색한다. title과 content 칼럼에 대소문자 구분 없이 포함 검사( contains)
@@ -24,5 +31,6 @@ class ListView(ListView, FormView):
         context = {}
         context['object_list'] = post_list # 검색된 결과를 컨텍스트 변수에 담는다
         context['search_word'] = word # 검색어를 컨텍스트 변수에 담는다
+        context['form'] = form_class
         return render(self.request, 'main/list.html', context)
-
+        
